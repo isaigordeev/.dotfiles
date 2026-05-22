3d16e691f9b16300c9b20390c4d4ae23828f69cf
Isai Gordeev <isaigordeev@gmail.com>
2026-05-22 10:45:42 +0200

    ruff in nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -249,6 +249,7 @@ return {
          require("conform").setup({
             formatters_by_ft = {
                markdown = { "prettier" },
+               python = { "ruff_format", "ruff_organize_imports" },
             },
             format_on_save = {
                timeout_ms = 500,
